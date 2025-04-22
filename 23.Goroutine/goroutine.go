ad7cfb25b6175f1931ad2269c7d201d58ff7f929
indura08 <induraperera3@gmail.com>
2025-04-22 21:43:30 +0530

    adding new notes codes related to go routine and channels lessons

--- 23.Goroutine/goroutine.go
+++ 23.Goroutine/goroutine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -14,7 +15,17 @@ func SaySomething() {
 func count(n string) {
 	for i := 1; i <= 3; i++ {
 		fmt.Println(n, ": ", i)
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(2000 * time.Millisecond)
+	}
+}
+
+// 3.waitgropup exmaple
+func SayHello(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Hello from say hello function which is a memeber og waitgroup")
+	for i := 1; i <= 5; i++ {
+		fmt.Println("value of i is :", i)
+		time.Sleep(2 * time.Second)
 	}
 }
 
@@ -24,10 +35,16 @@ func main() {
 	//time.Sleep(1 * time.Second) // sleep for 1 seconds to allow goroutine to finish
 
 	//2.example
-	go count("A")
-	go count("B")
-	count("C")
+	// go count("A")
+	// go count("B")
+	// count("C")
+
+	//3.example-waitgropu scenen ek
+	var wg sync.WaitGroup
+
+	wg.Add(1) //tell the gropu we are starting 1 goroutine
+	go SayHello(&wg)
+	wg.Wait() //wait for all go routines
+	fmt.Println("Main function is done")
 
 }
-
-//sync.wait heta code krnna one , waitgroups b;nna one
