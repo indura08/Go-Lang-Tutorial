31265eded8b8c2e6021a9a0d056314daa01b0bb5
indura08 <induraperera3@gmail.com>
2025-04-23 02:39:32 +0530

    adding codes for new lesson about imports and package differences

---
+++ 26.Import-case/A.go
@@ -0,0 +1,9 @@
+package main
+
+func MethodA(name string) string {
+	return "Hello " + name + " this is A file MethodA"
+}
+
+func MethodB(name string) string {
+	return "Hello " + name + " this is A file MethodB"
+}
---
+++ 26.Import-case/B.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(MethodA("Eliana"))
+	fmt.Println(MethodB("vivienne"))
+}
+
+// so api A.go eke methods use krna hinda B.go eke B.go eka thaniym run krnna bha A.go ekath run wenna one
+//so command: go run A.go B.go
